28d9d53fe597c35d394ff143cab6238c6d488352
Hiep Tran <tranquyhiep87@gmail.com>
2021-07-14 17:40:42 +0700

    Update code

--- magento-functional-tests/entrypoint.sh
+++ magento-functional-tests/entrypoint.sh
@@ -114,7 +114,7 @@ php bin/magento module:disable Magento_TwoFactorAuth
 php bin/magento cache:flush 
 
 echo "Start Selenium Server"
-selenium-standalone start --verbose --headless --no-sandbox --disable-gpu --disable-dev-shm-usage --disable-extensions --allow-running-insecure-content --ignore-certificate-errors --allow-insecure-localhost --disable-gpu &
+selenium-standalone start --headless --no-sandbox --disable-gpu --disable-dev-shm-usage --disable-extensions --allow-running-insecure-content --ignore-certificate-errors --allow-insecure-localhost &
 echo "Selenium checks"
 nc -z -w1 127.0.0.1 4444 || (echo "Selenium is not running" && exit)
 
